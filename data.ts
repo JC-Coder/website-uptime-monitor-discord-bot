d9fd0f5fe1febf93d6826d056ed2ea05cf07de51
JC-Coder <josephchimezie2003@gmail.com>
2025-08-22 14:27:10 +0100

    chore: update

--- data.ts
+++ data.ts
@@ -5,32 +5,32 @@ export const sitesToMonitor: SiteConfig[] = [
   {
     url: "https://ogaticket.com",
     name: "OgaTicket Production Landing Page",
-    expectedResponseTime: 3000, // 3 seconds
+    expectedResponseTime: 6000, // 6 seconds
   },
   {
     url: "https://watawi.ogaticket.com",
     name: "Watawi Website",
-    expectedResponseTime: 3000, // 3 seconds
+    expectedResponseTime: 6000, // 6 seconds
   },
 
   // Staging website
   {
     url: "https://staging-v1.ogaticket.com",
     name: "Ogaticket Staging V1 Website",
-    expectedResponseTime: 3000, // 3 seconds
+    expectedResponseTime: 6000, // 6 seconds
   },
 
   // Production API
   {
     url: "https://api-v1.ogaticket.com/api/setting",
     name: "OgaTicket Production API",
-    expectedResponseTime: 2000, // 2 seconds
+    expectedResponseTime: 6000, // 6 seconds
   },
 
   // Staging API
   {
     url: "https://api-staging-v1.ogaticket.com/api/setting",
     name: "OgaTicket Staging API",
-    expectedResponseTime: 2000, // 2 seconds
+    expectedResponseTime: 6000, // 6 seconds
   },
 ];
